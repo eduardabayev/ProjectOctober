d4ad9411110ed0dc2040fe3428c8595a9d93bc52
eduardabayev <73206176+eduardabayev@users.noreply.github.com>
2020-10-21 21:02:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function idCard(){
   var firstName= document.getElementById("firstName").value;
   var lastName= document.getElementById("lastName").value;
   var address= document.getElementById("address").value;
-  var Age= document.getElementById("Age").value;
+  var age= document.getElementById("Age").value;
   var phoneNumber= document.getElementById("phoneNumber").value;
   
   document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
   document.getElementById("postAddress").innerHTML = address;
-  document.getElementById("postAge").innerHTML = Age;
+  document.getElementById("postAge").innerHTML = age;
   document.getElementById("phoneNumber").innerHTML = phoneNumber;
 }
