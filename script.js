398fe36591a40524a954acca52a8c0be3e388865
eduardabayev <73206176+eduardabayev@users.noreply.github.com>
2020-10-22 22:16:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function idCard(){
   numberArray = [];
   numberArray.push(age);
   numberArray.push(phoneNumber);
-  var i;
-  for (i = 0; i < numberArray.length; i++) {
-    text += cars[i] + "<br>";
-  }
+  for (var i = 0; i < numberArray.length; i++);{
+    if (numberArray[i] <= 100){
+      document.getElementById("postAge").innerHTML = ("Age:" + " " + age);
+    }
+    else if (numberArray[i] > 100){
+      document.getElementById("postPhoneNumber").innerHTML = ("Phone Number" + " " + phoneNumber);
 
   document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
   document.getElementById("postAddress").innerHTML = address;
